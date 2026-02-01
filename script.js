e8cdd3eb92bd7cb150119ce02c00612b488daa76
Mahfuj <mdmahfuj.cse@gmail.com>
2026-02-02 00:09:57 +0600

    add js

---
+++ script.js
@@ -0,0 +1,24 @@
+// DOM Elements
+const timeDisplay = document.getElementById('timeDisplay');
+const dateDisplay = document.getElementById('dateDisplay');
+const formatToggle = document.getElementById('formatToggle');
+const timezoneToggle = document.getElementById('timezoneToggle');
+const snoozeBtn = document.getElementById('snoozeBtn');
+const alarmForm = document.getElementById('alarmForm');
+const alarmHour = document.getElementById('alarmHour');
+const alarmMinute = document.getElementById('alarmMinute');
+const alarmPeriod = document.getElementById('alarmPeriod');
+const alarmLabel = document.getElementById('alarmLabel');
+const alarmList = document.getElementById('alarmList');
+const themeToggle = document.getElementById('themeToggle');
+const notification = document.getElementById('notification');
+const notificationText = document.getElementById('notificationText');
+const alarmSound = document.getElementById('alarmSound');
+
+// App State
+let is24HourFormat = false;
+let currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+let alarms = [];
+let alarmCounter = 0;
+let activeAlarm = null;
+let isLightTheme = false;
